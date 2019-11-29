3e5ff01aa87b4d5c148ac60ccceaea2861a52d5e
Danilo G. Veraszto <garbini@gmail.com>
2019-11-29 16:42:24 -0300

    BashRCMechanism

---
+++ BashScripts/dot.bashrc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+## .bashrc 
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+clean_bash=${0#-}
+clean_bash=${clean_bash##*/}
+PS1="\u@$clean_bash\w\$ "
+unset PROMPT_COMMAND
+shopt -s autocd
+export TERM=xterm-256color
+HISTSIZE=-1
+HISTFILESIZE=-1
+
+source ~/git/GracefulGNU/BashScripts/powerline.load.sh
--- screen/launcher.sh
+++ screen/launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 test "$2" = "wrapper" &&\
-screen="screen -e '' -c ${HOME}/git/GracefulGNU/screen/combo.outer.conf -mS $1" ||\
-screen="screen -e ^ba -c ${HOME}/git/GracefulGNU/screen/combo.inner.conf -mS $1"
+screen="screen -e '' -c ${HOME}/git/GracefulGNU/screen/combo.outer.conf -mS $1 $3" ||\
+screen="screen -e ^ba -c ${HOME}/git/GracefulGNU/screen/combo.inner.conf -mS $1 $3"
 
-$screen
+echo $screen
