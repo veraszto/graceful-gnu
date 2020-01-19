994e1725ebab6044a9fa128e41f137fcfca4a8be
Danilo G. Veraszto <garbini@gmail.com>
2020-01-19 10:53:30 -0300

    Slight details

--- BashScripts/dot.bashrc.sh
+++ BashScripts/dot.bashrc.sh
@@ -2,6 +2,11 @@
 #
 ## .bashrc 
 # Source global definitions
+
+#This is set later
+
+test $BASHRCSOURCED || unset HISTFILE
+
 if [ -f /etc/bashrc ]; then
 	. /etc/bashrc
 fi
@@ -12,6 +17,8 @@ PS1="\u@$clean_bash\w\$ "
 unset PROMPT_COMMAND
 shopt -s autocd
 export TERM=xterm-256color
+
+
 HISTSIZE=-1
 HISTFILESIZE=-1
 
