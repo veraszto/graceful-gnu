1913799e0b447ee75c7922fb61f22c4aed504d84
Danilo G. Veraszto <veraszto@localhost.localdomain>
2020-03-27 12:19:18 -0300

    Inner screen podman

--- BashScripts/build.workspace/1.sh
+++ BashScripts/build.workspace/1.sh
@@ -5,7 +5,6 @@ gracefulgnu=$HOME/git/GracefulGNU/
 graceful_git_screen_launcher="$gracefulgnu/screen/launcher.sh"
 graceful_git_bashscript="$gracefulgnu/BashScripts/"
 
-#screen -e "''" -c $gracefulgnu/screen/MainLeft.conf -S Wrapper.MainLeft
 screen -e "''" -c $gracefulgnu/screen/MainRight.conf -S Wrapper.MainRight
 
 
