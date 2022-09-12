044bc218b1f4118e3f165e139ec5b1632b9d111e
Danilo G. Veraszto <garbini@gmail.com>
2022-09-12 09:52:10 -0300

    No projects initializer's config no problem

--- BashScripts/build.workspace/VimWithTmux.sh
+++ BashScripts/build.workspace/VimWithTmux.sh
@@ -27,23 +27,27 @@ do
 		echo "There is not a project in there"
 		continue
 	fi
-	sessionName=$(cat $path/config | grep "^name[[:space:]]" | sed -e 's/^name[[:space:]]\+//' | head -n1 2> /dev/null)
+	sessionName=$(cat $path/config 2> /dev/null | grep "^name[[:space:]]" | sed -e 's/^name[[:space:]]\+//' | head -n1 2> /dev/null)
 	if [ $? -gt 0 -o -z "$sessionName" ]
 	then
 		echo "Please add 'name <name>' to $path/config"
-		continue
 	fi
-	gitProjectPath=$(cat $path/config | grep "^path[[:space:]]" | sed -e 's/^path[[:space:]]\+//' | head -n1 2> /dev/null)
+	gitProjectPath=$(cat $path/config  2> /dev/null| grep "^path[[:space:]]" | sed -e 's/^path[[:space:]]\+//' | head -n1 2> /dev/null)
 	if [ $? -gt 0 -o -z "$gitProjectPath" ]
 	then
 		echo "Please add 'path <path>' to $path/config"
-		continue
 	fi
 	echo "Matched prefix: $project"
 	echo "Tmux session: $sessionName"
-	echo "Tmux project path: $gitProjectPath"
+	echo "Tmux project path: ${gitProjectPath:=~/}"
 
-	exec="tmux -f $MY_TMUX_CONF -S $MY_TMUX_SOCKET new-session -s $sessionName $tmuxSep "
+	buildSessionName="new-session $tmuxSep "
+	if [ -n "$sessionName" ]
+	then
+		buildSessionName="new-session -s $sessionName $tmuxSep "
+	fi
+
+	exec="tmux -f $MY_TMUX_CONF -S $MY_TMUX_SOCKET $buildSessionName"
 	eligible=$(find $path -iregex ".*\.vim$" | sort)
 	for file in $eligible 
 	do
@@ -66,15 +70,13 @@ do
 	addBashContext="new-window $MY_BASH_CONTEXT $project $tmuxSep "
 	#hold="${sum}${addBashContext}kill-window -t 0 $tmuxSep move-window -r $tmuxSep "
 	hold="${sum}kill-window -t 0 $tmuxSep move-window -r $tmuxSep "
-	hold="$hold set-buffer -b ${project}.loader.path $path $tmuxSep "
-	hold="$hold set-buffer -b ${project}.project.path $gitProjectPath $tmuxSep "
+	hold="$hold set-buffer -b ${project}.loader.path \"$path\" $tmuxSep "
+	hold="$hold set-buffer -b ${project}.project.path \"$gitProjectPath\" $tmuxSep "
 
 	run="$exec${hold:0:-3}"
-	unset hold
+	unset hold	
 	echo $run
-	echo
-
-	gnome-terminal --maximize --full-screen --title "$project" -- /bin/bash --login -c "$run"
+	$MY_GNOME_TERMINAL_PROJECTS_LAUNCHER_INITIALIZATOR --title "$project" -- /bin/bash --login -c "$run"
 
 done
 
