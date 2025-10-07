9917fdcdf2686e743582f21c043a411a2f565b0e
Danilo G. Veraszto <garbini@gmail.com>
2025-10-07 20:18:12 -0300

    A good computer

--- docker/a-good-computer/my-ubuntu/run-my-ubuntu.sh
+++ docker/a-good-computer/my-ubuntu/run-my-ubuntu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker container create --interactive --tty \
+#xhost
+
+docker run --rm -it \
 --network host \
 --volume /run/user/1000/wayland-0:/run/root/wayland-0 \
 --env GTK_BACKEND=wayland \
@@ -8,4 +10,8 @@ docker container create --interactive --tty \
 --env XDG_RUNTIME_DIR=/run/root \
 --env WAYLAND_DISPLAY=wayland-0 \
 --env DISPLAY=:0 \
-veraszto/my-basic-ubuntu:with-python
+--volume $MY_VOLUME_SENSITIVE_DATA \
+--volume $MY_VOLUME_E_DATA_CONFIG \
+--name my-ubuntu \
+veraszto/my-ubuntu
+exit
