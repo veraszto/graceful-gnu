354dc52942bb5e8ba51e2e37a43f0c43dd89fbf1
Danilo G. Veraszto <garbini@gmail.com>
2026-02-22 22:21:48 -0300

    Adjusted docker files

--- docker/a-good-computer/my-ubuntu/run-my-ubuntu.sh
+++ docker/a-good-computer/my-ubuntu/run-my-ubuntu.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+xhost +local:docker
+
+echo ${VOLUME_NAME:=my-volume}:${VOLUME_TARGET:=/root/volume}
+
 docker run --rm -it \
 --network host \
---volume /run/user/1000/wayland-0:/run/root/wayland-0 \
---env GTK_BACKEND=wayland \
---env QT_QPA_PLATFORM=wayland \
---env XDG_RUNTIME_DIR=/run/root \
---env WAYLAND_DISPLAY=wayland-0 \
---env DISPLAY=:0 \
---volume $MY_VOLUME_SENSITIVE_DATA \
---volume $MY_VOLUME_E_DATA_CONFIG \
+-e DISPLAY=$DISPLAY \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+--volume $VOLUME_NAME:$VOLUME_TARGET \
 --name my-ubuntu \
 veraszto/my-ubuntu
 exit
+
+#docker run --rm -it \
+#--network host \
+#--volume /run/user/1000/wayland-0:/run/root/wayland-0 \
+#--env GTK_BACKEND=wayland \
+#--env QT_QPA_PLATFORM=wayland \
+#--env XDG_RUNTIME_DIR=/run/root \
+#--env WAYLAND_DISPLAY=wayland-0 \
+#--env DISPLAY=:0 \
+#--volume $MY_VOLUME_SENSITIVE_DATA \
+#--volume $MY_VOLUME_E_DATA_CONFIG \
+#--name my-ubuntu \
+#veraszto/my-ubuntu
+#exit
